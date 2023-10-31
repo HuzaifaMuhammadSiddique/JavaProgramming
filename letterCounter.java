b0f64084c044be444151d14ab2fbf8dac6a1d6a9
Huzaifa Muhammad Siddique <huzaifasid02@gmail.com>
2023-10-31 23:08:13 +0300

    Create letterCounter.java

---
+++ letterCounter.java
@@ -0,0 +1,22 @@
+/*
+Write a Java program to count the number of occurrences of a specific character in a given string.
+*/
+
+class LetterCounter {
+    public static int letterCounter(String word, char letter){
+        int counter = 0;
+        for(int i=0; i<word.length(); i++){
+            if(word.charAt(i) == letter){
+                counter++;
+            } 
+        }
+        return counter;
+    }
+    
+    public static void main(String[] args) {
+        int result = letterCounter("My name is Huzaifa and who are you?", "a");
+        System.out.println(result);
+    }
+    
+    
+}
